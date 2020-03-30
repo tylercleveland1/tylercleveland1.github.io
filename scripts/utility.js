dfd676b332d77fdabcb3ee0d268eb05e309ba247
Tyler Cleveland <tyler@focal.ws>
2020-03-29 22:32:44 -0700

    minor changes

--- scripts/utility.js
+++ scripts/utility.js
@@ -1,3 +1,18 @@
+//foremost append all apprioriate scripts to to of page
+var jq = document.createElement('script');
+jq.setAttribute('src', 'https://code.jquery.com/jquery-3.4.1.min.js');
+var popper = document.createElement('script');
+popper.setAttribute('src', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js');
+var bs = document.createElement('script');
+bs.setAttribute('src', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js');
+var bscss = document.createElement('link');
+bscss.setAttribute('href', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css');
+
+document.appendChild(jq);
+document.appendChild(popper);
+document.appendChild(bs);
+document.appendChild(bscss);
+
 var navItems = {
     About: '/index.html'
     , Projects: '/projects.html'
