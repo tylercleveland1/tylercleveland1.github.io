216b86e6b198594f0e22027ff16f2720ad90ce94
Tyler Cleveland <tyler@focal.ws>
2020-03-29 20:51:46 -0700

    minor changes

---
+++ scripts/utility.js
@@ -0,0 +1,25 @@
+var navItems = {
+    About: '/index.html'
+    , Projects: '/projects.html'
+    , Contact: '/contact.html'
+}
+
+$.fn.loadNavbar = function (activeNavItem = navItems.About) {
+    var $navContainer = $(this);
+    $navContainer.empty();
+    $navContainer.load('/navbar-template.html');
+
+    var $navTemplate = $navContainer.find('[nav-template]');
+    var $navUl = $navTemplate.closest('ul');
+    
+    $navUl.empty();
+
+    for (navKey in navItems) {
+        var $thisNavItem = $navTemplate.clone();
+        $thisNavItem.find('a').attr('href', navItems[navKey]);
+        if (activeNavItem === navItems[navKey]) {
+            thisNavItem.addClass('active');
+        }
+        $navUl.append($thisNavItem);
+    }
+}
