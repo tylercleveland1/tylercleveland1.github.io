4968c4acd9df8a682d9d6db206317532477a820a
Tyler Cleveland <tyler@focal.ws>
2020-03-29 21:51:36 -0700

    minor changes

--- scripts/utility.js
+++ scripts/utility.js
@@ -6,21 +6,21 @@ var navItems = {
 
 $.fn.loadNavbar = function (activeNavItem = navItems.About) {
     var $navContainer = $(this);
-    $navContainer.load('/navbar-template.html');
+    $navContainer.load('/navbar-template.html', function () {
+        var $navTemplate = $navContainer.find('[nav-template]');
+        var navTemplateHtml = $navTemplate.prop('outerHTML');
+        var $navUl = $navTemplate.closest('ul');
 
-    var $navTemplate = $navContainer.find('[nav-template]');
-    var navTemplateHtml = $navTemplate.prop('outerHTML');
-    var $navUl = $navTemplate.closest('ul');
+        $navUl.empty();
 
-    $navUl.empty();
-
-    for (navKey in navItems) {
-        var $thisNavItem = $(navTemplateHtml);
-        $thisNavItem.find('a').attr('href', navItems[navKey]);
-        $thisNavItem.find('a').text(navKey);
-        if (activeNavItem === navItems[navKey]) {
-            $thisNavItem.addClass('active');
+        for (navKey in navItems) {
+            var $thisNavItem = $(navTemplateHtml);
+            $thisNavItem.find('a').attr('href', navItems[navKey]);
+            $thisNavItem.find('a').text(navKey);
+            if (activeNavItem === navItems[navKey]) {
+                $thisNavItem.addClass('active');
+            }
+            $navUl.append($thisNavItem);
         }
-        $navUl.append($thisNavItem);
-    }
+    });
 }
