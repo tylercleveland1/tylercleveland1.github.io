9b69ee6cf40ffdae0cf16d56b9b67fa0fab9c20d
Tyler Cleveland <tyler@focal.ws>
2020-05-18 21:26:07 -0700

    test

--- scripts/utility.js
+++ scripts/utility.js
@@ -25,7 +25,7 @@ var projects = [
     , {
         title: 'Regression Using Fourier Series (Part 1)'
         , htmllink: '/projects/fourier-regressor-part1.html'
-    }}
+    }
     // , {
     //     title: 'Applying Artificial Neural Networks for Human Activity Recognition'
     //     , htmllink: '/projects/ann-har.html'
